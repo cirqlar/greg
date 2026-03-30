d34480ba012c831fc05b45c2647e2abc6aa43602
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2026-03-30 11:49:26 +0100

    add tests for login queries
    add shared db fixtures that tests can use

--- src/server/auth/queries/login.rs
+++ src/server/auth/queries/login.rs
@@ -40,3 +40,97 @@ pub async fn get_key_timestamp(
         Ok(None)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use libsql::{Connection, Value};
+    use rstest::rstest;
+
+    use super::*;
+    use crate::db::tests::empty_db;
+
+    #[rstest]
+    #[tokio::test]
+    async fn can_save_login_key(#[future(awt)] empty_db: Connection) -> Result<(), DatabaseError> {
+        let id = Uuid::new_v4();
+
+        save_login_id(&empty_db, &id).await?;
+
+        let mut rows = empty_db
+            .query(
+                &format!("SELECT key FROM {LOGINS_T} WHERE key = ?1"),
+                [id.to_string()],
+            )
+            .await?;
+
+        let Some(row) = rows.next().await? else {
+            panic!("Did not find key in database");
+        };
+
+        let Value::Text(id_from_db) = row.get_value(0)? else {
+            panic!("Key isn't text");
+        };
+
+        assert_eq!(id_from_db, id.to_string());
+
+        Ok(())
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn can_not_save_duplicate_login_key(
+        #[future(awt)] empty_db: Connection,
+    ) -> Result<(), DatabaseError> {
+        let id = Uuid::new_v4();
+
+        save_login_id(&empty_db, &id).await?;
+        let second_try = save_login_id(&empty_db, &id).await;
+
+        assert!(second_try.is_err());
+
+        let mut rows = empty_db
+            .query(
+                &format!("SELECT key FROM {LOGINS_T} WHERE key = ?1"),
+                [id.to_string()],
+            )
+            .await?;
+
+        let Some(_) = rows.next().await? else {
+            panic!("Did not find key in database");
+        };
+
+        assert!(rows.next().await?.is_none());
+
+        Ok(())
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn can_retrieve_timestamp_for_key(
+        #[future(awt)] empty_db: Connection,
+    ) -> Result<(), DatabaseError> {
+        let id = Uuid::new_v4();
+
+        save_login_id(&empty_db, &id).await?;
+
+        let timestamp = get_key_timestamp(&empty_db, &id.to_string()).await?;
+
+        assert!(timestamp.is_some());
+
+        Ok(())
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn missing_key_does_not_error(
+        #[future(awt)] empty_db: Connection,
+    ) -> Result<(), DatabaseError> {
+        let id = Uuid::new_v4();
+
+        let timestamp = get_key_timestamp(&empty_db, &id.to_string()).await?;
+
+        assert!(timestamp.is_none());
+
+        Ok(())
+    }
+}
--- src/server/db/mod.rs
+++ src/server/db/mod.rs
@@ -5,3 +5,45 @@ mod types;
 
 pub use get::{GetDatabaseError, get_database, get_demo_database};
 pub use migrations::{ApplyMigrationError, apply_migrations};
+
+#[cfg(test)]
+pub mod tests {
+    use libsql::{Builder, Connection};
+    use rstest::fixture;
+
+    use super::*;
+
+    async fn fill_db(db: &Connection, dump_path: &str) {
+        use std::env::var;
+        use std::path::PathBuf;
+
+        let mut full_path = PathBuf::from(var("CARGO_MANIFEST_DIR").expect("has manifest dir"));
+        full_path.push(dump_path);
+        let sql = std::fs::read_to_string(full_path).expect("dump file exists");
+
+        db.execute_batch(&sql).await.expect("Can fill database");
+    }
+
+    #[fixture]
+    pub async fn empty_db() -> Connection {
+        let db = Builder::new_local(":memory:")
+            .build()
+            .await
+            .unwrap()
+            .connect()
+            .expect("Can connect to in memory database");
+
+        apply_migrations(db.clone())
+            .await
+            .expect("Can migrate in memory database");
+
+        db
+    }
+
+    #[fixture]
+    pub async fn filled_db(#[future(awt)] empty_db: Connection) -> Connection {
+        fill_db(&empty_db, "tests/dumps/v2_dump.sql").await;
+
+        empty_db
+    }
+}
