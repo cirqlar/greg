ac78dab2f8b6fec0065b3ca97a067267ef91733f
Ayanfe Ibitoye <ibitoyeayanfeoluwa@gmail.com>
2026-04-23 17:20:42 +0100

    add some tests for check_source in rss check task

--- src/server/rss/tasks/check/check_source.rs
+++ src/server/rss/tasks/check/check_source.rs
@@ -167,3 +167,418 @@ pub async fn check_source(
         Ok(Some(CheckReturn(info, entries)))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+    use time::{ext::NumericalDuration, format_description::well_known};
+
+    use super::*;
+
+    #[rstest]
+    #[tokio::test]
+    async fn check_source_returns_only_posts_past_time() -> Result<(), CheckError> {
+        let mut server = mockito::Server::new_async().await;
+        let m1 = server.mock("GET", "/").with_body(format!(
+            r##"
+                <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+                    <channel>
+                        <title>Test Source</title>
+                        <link/>
+                        <description/>
+                        <updated>{pub_date_1}</updated>
+                        <atom:link href="https://test_source.com" rel="self" type="application/rss+xml"/>
+                        <item>
+                            <title>Item 1</title>
+                            <link>https://test_source.com/item_1</link>
+                            <summary>Item 1 summary</summary>
+                            <description>Item 1 description</description>
+                            <category>Dummy category</category>
+                            <guid>https://test_source.com/item_1</guid>
+                            <dc:creator>Source creator</dc:creator>
+                            <pubDate>{pub_date_1}</pubDate>
+                            <image>https://test_source.com/item_1.jpg</image>
+                        </item>
+                        <item>
+                            <title>Item 2</title>
+                            <link>https://test_source.com/item_2</link>
+                            <summary>Item 2 summary</summary>
+                            <description>Item 2 description</description>
+                            <category>Dummy category</category>
+                            <guid>https://test_source.com/item_2</guid>
+                            <dc:creator>Source creator</dc:creator>
+                            <pubDate>{pub_date_2}</pubDate>
+                            <image>https://test_source.com/item_2.jpg</image>
+                        </item>
+                    </channel>
+                </rss>
+            "##, 
+            pub_date_1 = (OffsetDateTime::now_utc() - 1.hours()).format(&well_known::Rfc2822).expect("can format datetime"),
+            pub_date_2 = (OffsetDateTime::now_utc() - 3.hours()).format(&well_known::Rfc2822).expect("can format datetime")
+        )).create_async().await;
+
+        let source = Source {
+            id: 0,
+            url: server.url(),
+            last_checked: (OffsetDateTime::now_utc() - 2.hours()),
+            enabled: true,
+            failed_count: 0,
+        };
+
+        let ret = check_source(&source, reqwest::Client::new()).await?;
+
+        assert!(ret.is_some());
+
+        let CheckReturn(info, posts) = ret.unwrap();
+
+        assert_eq!(info.source_id, source.id);
+        assert_eq!(info.source_url, source.url);
+        assert_eq!(info.channel_title.as_str(), "Test Source");
+        let difference = OffsetDateTime::now_utc() - info.most_recent;
+        let compared_to_actual = difference - 1.hours();
+        assert!(compared_to_actual.abs() < 5.minutes());
+
+        assert_eq!(posts.len(), 1);
+        assert_eq!(posts[0].title.as_str(), "Item 1");
+
+        m1.assert_async().await;
+
+        Ok(())
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn check_source_returns_none_when_all_posts_are_too_old() -> Result<(), CheckError> {
+        let mut server = mockito::Server::new_async().await;
+        let m1 = server.mock("GET", "/").with_body(format!(
+            r##"
+                <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+                    <channel>
+                        <title>Test Source</title>
+                        <link/>
+                        <description/>
+                        <updated>{pub_date_1}</updated>
+                        <atom:link href="https://test_source.com" rel="self" type="application/rss+xml"/>
+                        <item>
+                            <title>Item 1</title>
+                            <link>https://test_source.com/item_1</link>
+                            <summary>Item 1 summary</summary>
+                            <description>Item 1 description</description>
+                            <category>Dummy category</category>
+                            <guid>https://test_source.com/item_1</guid>
+                            <dc:creator>Source creator</dc:creator>
+                            <pubDate>{pub_date_1}</pubDate>
+                            <image>https://test_source.com/item_1.jpg</image>
+                        </item>
+                        <item>
+                            <title>Item 2</title>
+                            <link>https://test_source.com/item_2</link>
+                            <summary>Item 2 summary</summary>
+                            <description>Item 2 description</description>
+                            <category>Dummy category</category>
+                            <guid>https://test_source.com/item_2</guid>
+                            <dc:creator>Source creator</dc:creator>
+                            <pubDate>{pub_date_2}</pubDate>
+                            <image>https://test_source.com/item_2.jpg</image>
+                        </item>
+                    </channel>
+                </rss>
+            "##, 
+            pub_date_1 = (OffsetDateTime::now_utc() - 125.minutes()).format(&well_known::Rfc2822).expect("can format datetime"),
+            pub_date_2 = (OffsetDateTime::now_utc() - 3.hours()).format(&well_known::Rfc2822).expect("can format datetime")
+        )).create_async().await;
+
+        let source = Source {
+            id: 0,
+            url: server.url(),
+            last_checked: (OffsetDateTime::now_utc() - 1.hours()),
+            enabled: true,
+            failed_count: 0,
+        };
+
+        let ret = check_source(&source, reqwest::Client::new()).await?;
+
+        assert!(ret.is_none());
+
+        m1.assert_async().await;
+
+        Ok(())
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn check_source_returns_none_for_disabled_source() -> Result<(), CheckError> {
+        let mut server = mockito::Server::new_async().await;
+        let m1 = server.mock("GET", "/").with_body(
+            r##"
+                <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+                    <channel>
+                        <title>Test Source</title>
+                        <link/>
+                        <description/>
+                        <atom:link href="https://test_source.com" rel="self" type="application/rss+xml"/>
+                    </channel>
+                </rss>
+            "## 
+        ).create_async().await.expect(0);
+
+        let source = Source {
+            id: 0,
+            url: server.url(),
+            last_checked: (OffsetDateTime::now_utc() - 2.hours()),
+            enabled: false,
+            failed_count: 0,
+        };
+
+        let ret = check_source(&source, reqwest::Client::new()).await?;
+
+        assert!(ret.is_none());
+
+        m1.assert_async().await;
+
+        Ok(())
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn check_source_can_handle_multiple_links() -> Result<(), CheckError> {
+        let mut server = mockito::Server::new_async().await;
+        let m1 = server.mock("GET", "/").with_body(format!(
+            r##"
+                <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+                    <channel>
+                        <title>Test Source</title>
+                        <link/>
+                        <description/>
+                        <atom:link href="https://test_source.com" rel="self" type="application/rss+xml"/>
+                        <item>
+                            <title>Item 1</title>
+                            <link>https://test_source.com/item_1</link>
+                            <link>https://test_source.com/item_1_alt</link>
+                            <summary>Item 1 summary</summary>
+                            <description>Item 1 description</description>
+                            <category>Dummy category</category>
+                            <guid>https://test_source.com/item_1</guid>
+                            <dc:creator>Source creator</dc:creator>
+                            <pubDate>{pub_date_1}</pubDate>
+                            <image>https://test_source.com/item_1.jpg</image>
+                        </item>
+                    </channel>
+                </rss>
+            "##, 
+            pub_date_1 = (OffsetDateTime::now_utc() - 1.hours()).format(&well_known::Rfc2822).expect("can format datetime")
+        )).create_async().await;
+
+        let source = Source {
+            id: 0,
+            url: server.url(),
+            last_checked: (OffsetDateTime::now_utc() - 2.hours()),
+            enabled: true,
+            failed_count: 0,
+        };
+
+        let ret = check_source(&source, reqwest::Client::new()).await?;
+
+        assert!(ret.is_some());
+
+        let CheckReturn(info, posts) = ret.unwrap();
+
+        assert_eq!(info.source_id, source.id);
+        assert_eq!(info.source_url, source.url);
+        assert_eq!(info.channel_title.as_str(), "Test Source");
+        let difference = OffsetDateTime::now_utc() - info.most_recent;
+        let compared_to_actual = difference - 1.hours();
+        assert!(compared_to_actual.abs() < 5.minutes());
+
+        assert_eq!(posts.len(), 1);
+        assert_eq!(posts[0].title.as_str(), "Item 1");
+        assert_eq!(posts[0].url.as_str(), "https://test_source.com/item_1");
+
+        m1.assert_async().await;
+
+        Ok(())
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn check_source_can_handle_no_links() -> Result<(), CheckError> {
+        let mut server = mockito::Server::new_async().await;
+        let m1 = server.mock("GET", "/").with_body(format!(
+            r##"
+                <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+                    <channel>
+                        <title>Test Source</title>
+                        <link/>
+                        <description/>
+                        <atom:link href="https://test_source.com" rel="self" type="application/rss+xml"/>
+                        <item>
+                            <title>Item 1</title>
+                            <summary>Item 1 summary</summary>
+                            <description>Item 1 description</description>
+                            <category>Dummy category</category>
+                            <guid>https://test_source.com/item_1</guid>
+                            <dc:creator>Source creator</dc:creator>
+                            <pubDate>{pub_date_1}</pubDate>
+                            <image>https://test_source.com/item_1.jpg</image>
+                        </item>
+                    </channel>
+                </rss>
+            "##, 
+            pub_date_1 = (OffsetDateTime::now_utc() - 1.hours()).format(&well_known::Rfc2822).expect("can format datetime")
+        )).create_async().await;
+
+        let source = Source {
+            id: 0,
+            url: server.url(),
+            last_checked: (OffsetDateTime::now_utc() - 2.hours()),
+            enabled: true,
+            failed_count: 0,
+        };
+
+        let ret = check_source(&source, reqwest::Client::new()).await?;
+
+        assert!(ret.is_some());
+
+        let CheckReturn(info, posts) = ret.unwrap();
+
+        assert_eq!(info.source_id, source.id);
+        assert_eq!(info.source_url, source.url);
+        assert_eq!(info.channel_title.as_str(), "Test Source");
+        let difference = OffsetDateTime::now_utc() - info.most_recent;
+        let compared_to_actual = difference - 1.hours();
+        assert!(compared_to_actual.abs() < 5.minutes());
+
+        assert_eq!(posts.len(), 1);
+        assert_eq!(posts[0].title.as_str(), "Item 1");
+        assert_eq!(posts[0].url.as_str(), "No Url");
+
+        m1.assert_async().await;
+
+        Ok(())
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn check_source_can_handle_no_summary() -> Result<(), CheckError> {
+        let mut server = mockito::Server::new_async().await;
+        let m1 = server.mock("GET", "/").with_body(format!(
+            r##"
+                <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+                    <channel>
+                        <title>Test Source</title>
+                        <link/>
+                        <description/>
+                        <updated>{pub_date_1}</updated>
+                        <atom:link href="https://test_source.com" rel="self" type="application/rss+xml"/>
+                        <item>
+                            <title>Item 1</title>
+                            <link>https://test_source.com/item_1</link>
+                            <description>Item 1 description</description>
+                            <category>Dummy category</category>
+                            <guid>https://test_source.com/item_1</guid>
+                            <dc:creator>Source creator</dc:creator>
+                            <pubDate>{pub_date_1}</pubDate>
+                            <image>https://test_source.com/item_1.jpg</image>
+                        </item>
+                    </channel>
+                </rss>
+            "##, 
+            pub_date_1 = (OffsetDateTime::now_utc() - 1.hours()).format(&well_known::Rfc2822).expect("can format datetime")
+        )).create_async().await;
+
+        let source = Source {
+            id: 0,
+            url: server.url(),
+            last_checked: (OffsetDateTime::now_utc() - 2.hours()),
+            enabled: true,
+            failed_count: 0,
+        };
+
+        let ret = check_source(&source, reqwest::Client::new()).await?;
+
+        assert!(ret.is_some());
+
+        let CheckReturn(info, posts) = ret.unwrap();
+
+        assert_eq!(info.source_id, source.id);
+        assert_eq!(info.source_url, source.url);
+        assert_eq!(info.channel_title.as_str(), "Test Source");
+        let difference = OffsetDateTime::now_utc() - info.most_recent;
+        let compared_to_actual = difference - 1.hours();
+        assert!(compared_to_actual.abs() < 5.minutes());
+
+        assert_eq!(posts.len(), 1);
+        assert_eq!(posts[0].title.as_str(), "Item 1");
+        assert_eq!(posts[0].body.as_str(), "Item 1 description");
+
+        m1.assert_async().await;
+
+        Ok(())
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn check_source_can_handle_malformed_feed() -> Result<(), CheckError> {
+        let mut server = mockito::Server::new_async().await;
+        let m1 = server.mock("GET", "/").with_body(format!(
+            r##"
+                <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+                    <channel>
+                        <title>Test Source</title>
+                        <link/>
+                        <description/>
+                        <updated>{pub_date_1}</updated>
+                        <atom:link href="https://test_source.com" rel="self" type="application/rss+xml"/>
+                        <item>
+                            <title>Item 1</title>
+                            <link>https://test_source.com/item_1</link>
+            "##, 
+            pub_date_1 = (OffsetDateTime::now_utc() - 1.hours()).format(&well_known::Rfc2822).expect("can format datetime")
+        )).create_async().await;
+
+        let source = Source {
+            id: 0,
+            url: server.url(),
+            last_checked: (OffsetDateTime::now_utc() - 2.hours()),
+            enabled: true,
+            failed_count: 0,
+        };
+
+        let ret = check_source(&source, reqwest::Client::new()).await;
+
+        assert!(ret.is_err());
+        assert!(matches!(ret, Err(CheckError::Parse(_))));
+
+        m1.assert_async().await;
+
+        Ok(())
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn check_source_can_handle_failed_request() -> Result<(), CheckError> {
+        let mut server = mockito::Server::new_async().await;
+        let m1 = server
+            .mock("GET", "/")
+            .with_status(500)
+            .create_async()
+            .await;
+
+        let source = Source {
+            id: 0,
+            url: server.url(),
+            last_checked: (OffsetDateTime::now_utc() - 2.hours()),
+            enabled: true,
+            failed_count: 0,
+        };
+
+        let ret = check_source(&source, reqwest::Client::new()).await;
+
+        assert!(ret.is_err());
+        assert!(matches!(ret, Err(CheckError::Source(_))));
+
+        m1.assert_async().await;
+
+        Ok(())
+    }
+}
